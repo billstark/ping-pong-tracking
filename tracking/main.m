15167a902e9cce72d90a13b3394a75ddb6528901
Wu Zefeng <flamesdesperado@gmail.com>
2018-11-03 03:35:20 +0800

    MATLAB is shit shit shit

--- tracking/main.m
+++ tracking/main.m
@@ -3,34 +3,48 @@ addpath('./');
 close all
 clear all
 
+% Read video
+
 VName = 'CAM1-GOPR0333-21157.mp4';
 v = VideoReader(VName);
 v1 = VideoReader(VName);
+v2 = VideoReader(VName);
+numberOfFrames = v2.numberOfFrames;
+
+
+
+% Calculate averaged for background removal
 
 averaged = double(readFrame(v1));
-for i = 1 : floor(v1.FrameRate * v1.Duration - 1)
+uint8_avg = uint8(averaged);
+for i = 1 : (numberOfFrames-1)
     currentFrame = double(readFrame(v1));
     averaged = ((i / (i + 1)) .* averaged) + ((1 / (i + 1)) .* currentFrame);
 end
-imshow(uint8(averaged));
+imshow(uint8_avg);
 print('background.jpg', '-djpeg');
+
+
+
+% find corners in all frames
 
 firstFrame = readFrame(v);
 secondFrame = readFrame(v);
 
-for n = 1:floor(v.FrameRate * v.Duration - 1)
-    firstFrame = firstFrame - uint8(averaged);
-    secondFrame = secondFrame - uint8(averaged);
+for n = 1:(numberOfFrames - 2)
+    firstFrame = firstFrame - uint8_avg;
+    secondFrame = secondFrame - uint8_avg;
     [r, c] = findCorner(firstFrame, 13, 5);
 
-    imshow(firstFrame + uint8(averaged));
-    hold on;
-    for i = 1:size([r; c]', 1)
-        x = r(i);
-        y = c(i);
-        rectangle('Position', [x - 6, y - 6, 13, 13], 'EdgeColor', 'r', 'LineWidth', 1)
-    end
-    print(strcat('f', num2str(n), '.jpg'), '-djpeg');
-    firstFrame = secondFrame + uint8(averaged);
+    % imshow(firstFrame + uint8_avg);
+    % hold on;
+    % for i = 1:size([r; c]', 1)
+    %     x = r(i);
+    %     y = c(i);
+    %     rectangle('Position', [x - 6, y - 6, 13, 13], 'EdgeColor', 'r', 'LineWidth', 1)
+    % end
+    % print(strcat('f', num2str(n), '.jpg'), '-djpeg');
+
+    firstFrame = secondFrame + uint8_avg;
     secondFrame = readFrame(v);
 end
