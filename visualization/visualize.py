13c005a7796544c69fc1d40165df86bbac656f40
Wu Zefeng <flamesdesperado@gmail.com>
2018-11-14 01:54:02 +0800

    small fixes

--- visualization/visualize.py
+++ visualization/visualize.py
@@ -37,24 +37,6 @@ def read_camera_data():
     return cam_data_lst
 
 
-def R_to_euler_angles(R):
-    R = np.array(R)
-    sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
-     
-    singular = sy < 1e-6
- 
-    if  not singular :
-        x = math.atan2(R[2,1] , R[2,2])
-        y = math.atan2(-R[2,0], sy)
-        z = math.atan2(R[1,0], R[0,0])
-    else :
-        x = math.atan2(-R[1,2], R[1,1])
-        y = math.atan2(-R[2,0], sy)
-        z = 0
- 
-    return [x, y, z]
-
-
 def read_p_lst(file_name):
     points = [[float(y.strip()) for y in x.split(',')] for x in open(file_name, 'r').read().strip().split('\n')]
     return points
@@ -66,9 +48,6 @@ def plot(p_lst):
     mpl.rcParams['legend.fontsize'] = 10
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    ax.set_xlim([min(x), max(x)])
-    ax.set_ylim([min(y), max(y)])
-    ax.set_zlim([min(z), max(z)])
     for i in range(len(x)-1):
         a = Arrow3D([x[i], x[i+1]],[y[i], y[i+1]],[z[i], z[i+1]], mutation_scale=5, arrowstyle="->", color="r")
         ax.add_artist(a)
@@ -76,16 +55,58 @@ def plot(p_lst):
     
     # Plot camera data
     cam_data = read_camera_data()
+
+    cam_pos_lst = []
+
     for cam in cam_data:
         cam_pos = cam['pos']
+        cam_pos_lst.append(cam_pos)
         R = cam['R']
-        rx, ry, rz = R_to_euler_angles(R)
+        scale = 0.4
+        I = [row[0]*scale for row in R]
+        J = [row[1]*scale for row in R]
+        K = [row[2]*scale for row in R]
+        center_vec = [I[0]+J[0]+K[0], I[1]+J[1]+K[1], I[2]+J[2]+K[2]]
+        
+        I_arrow = Arrow3D([cam_pos[0], cam_pos[0] + I[0]],[cam_pos[1], cam_pos[1] + I[1]],[cam_pos[2], cam_pos[2] + I[2]], mutation_scale=5, arrowstyle="->", color="b")
+        ax.add_artist(I_arrow)
+        J_arrow = Arrow3D([cam_pos[0], cam_pos[0] + J[0]],[cam_pos[1], cam_pos[1] + J[1]],[cam_pos[2], cam_pos[2] + J[2]], mutation_scale=5, arrowstyle="->", color="b")
+        ax.add_artist(J_arrow)
+        K_arrow = Arrow3D([cam_pos[0], cam_pos[0] + K[0]],[cam_pos[1], cam_pos[1] + K[1]],[cam_pos[2], cam_pos[2] + K[2]], mutation_scale=5, arrowstyle="->", color="b")
+        ax.add_artist(K_arrow)
+        center_arrow = Arrow3D([cam_pos[0], cam_pos[0] + center_vec[0]],[cam_pos[1], cam_pos[1] + center_vec[1]],[cam_pos[2], cam_pos[2] + center_vec[2]], mutation_scale=5, arrowstyle="->", color="g")
+        ax.add_artist(center_arrow)
+
+    cam_pos_x = [x[0] for x in cam_pos_lst]
+    cam_pos_y = [x[1] for x in cam_pos_lst]
+    cam_pos_z = [x[2] for x in cam_pos_lst]
+
+    x_lst = cam_pos_x + list(x)
+    y_lst = cam_pos_y + list(y)
+    z_lst = cam_pos_z + list(z)
+
+    min_x = min(x_lst)
+    max_x = max(x_lst)
+    min_y = min(y_lst)
+    max_y = max(y_lst)
+    min_z = min(z_lst)
+    max_z = max(z_lst)
+
+    span = max([max_x-min_x, max_y-min_y, max_z-min_z])
+
+    ax.set_xlim([min_x, min_x+span])
+    ax.set_ylim([min_y, min_y+span])
+    ax.set_zlim([min_z, min_z+span])
+
         
 
 
 
     plt.show()
 
+
+def vector_mul(v1, v2):
+    return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
